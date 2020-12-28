94812d7983e94999bc767dafe9fcbfef73d9d42a
Veeresh Nerlige <veereshnm@gmail.com>
2020-12-28 16:58:38 -0600

    added card style to calendar list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
-//import gapi from ''
+import 'materialize-css/dist/css/materialize.min.css';
+import M from "materialize-css";
+
 
 // Client ID and API key from the Developer Console
 var CLIENT_ID = '480305113859-vnq89kvr7gc6dpjnu7tlooi78p3uqn1h.apps.googleusercontent.com';
 var API_KEY = 'AIzaSyCPdVPm6hpofGUXPwruA2XtuRnit1XOSb8';
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
- // included, separated by spaces.
+// included, separated by spaces.
 var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 //TODO:
 //https://www.npmjs.com/package/react-google-calendar-api
@@ -27,7 +29,7 @@ function initClient(signinButton, signoutButton, onGetCalendars) {
     updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get(), signinButton, signoutButton, onGetCalendars);
     signinButton.onclick = handleAuthClick;
     signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
+  }, function (error) {
     console.log(`Error : ${error}`)
   });
 }
@@ -41,65 +43,122 @@ function handleAuthClick(event) {
   window.gapi.auth2.getAuthInstance().signIn();
 }
 
-function updateSigninStatus(isSignedIn, signinButton, signoutButton,onGetCalendars) {
+function updateSigninStatus(isSignedIn, signinButton, signoutButton, onGetCalendars) {
+  console.log(`isSignedIn : ${isSignedIn}`);
+
   if (isSignedIn) {
     signinButton.style.display = 'none';
-    signoutButton.style.display = 'block';
+    signoutButton.style.display = 'inline-block';
     console.log('updateSigninStatus: isSignedIn');
-    window.gapi.client.calendar.calendarList.list({}).then(onGetCalendars, (err)=>{ console.log(`Error occured when retrieving calendar list :\n ${err}`)});
+    window.gapi.client.calendar.calendarList.list({}).then(onGetCalendars, (err) => { console.log(`Error occured when retrieving calendar list :\n ${err}`) });
   } else {
-    signinButton.style.display = 'block';
+    signinButton.style.display = 'inline-block';
     signoutButton.style.display = 'none';
+    onGetCalendars({result: {list: null}})
   }
 }
 
-
-function GCalendarList({calendars}){
-  console.log('component GCalendarList rendered');
-  console.log(calendars);
-  return (<div>
-    {calendars ? 
-      calendars.map((item) => {
-            return <div key={item.id}>{item.summaryOverride ? item.summaryOverride : item.summary}</div>
-          }) : 
-      <div>Loading the calendars...</div>
-    }
-  </div>);
+const Events = ({eventId}) => {
+  return (<div>Showing events for {eventId}</div>);
 }
 
 
+function GCalendarList({ calendars }) {
+  console.log('component GCalendarList rendered');
+  console.log(calendars);
+  return (<div>
+    <div>
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Calendars</span>
+          {(calendars && calendars.length > 0) ?
+              <ul class="collapsible">
+              {
+              calendars.map((item) => {
+                      return <li key={item.id}>
+                          <div class="collapsible-header black-text">{item.summaryOverride ? item.summaryOverride : item.summary}</div>
+                          <div class="collapsible-body">
+                            <Events eventId={item.id}></Events>
+                          </div>
+                        </li>
+                    })
+                    }
+              </ul>
+       :
+      <div>Please signin to display calendars</div>
+    }
+        </div>
+      </div>
+    </div>
+    
+  </div>);
+}
+
+function EventList() {
+  //get the calender id saved in local store.
+  //load the re
+
+  return (<div>
+
+  </div>);
+}
+
+const Navigation = ({ signinButton, signoutButton }) => {
+  return (
+    <nav>
+      <div class="nav-wrapper orange darken-4">
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            {/* eslint-disable-next-line*/}
+            <a id="signin" ref={signinButton} className="waves-effect waves-light btn">Sign in</a>
+          </li>
+          <li>
+            {/* eslint-disable-next-line*/}
+            <a id="signout" ref={signoutButton} className="waves-effect waves-light btn">Sign out</a>
+
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 function App() {
 
   console.log('App rerendered');
-  
+
   const [calendars, setCalendars] = React.useState();
 
   var googleSigningButton = React.useRef();
   var googleSignoutButton = React.useRef();
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
+    M.AutoInit();
     console.log('App - useEffect is called');
     const googleApiScript = document.createElement('script');
-    googleApiScript.src='https://apis.google.com/js/api.js';
+    googleApiScript.src = 'https://apis.google.com/js/api.js';
     googleApiScript.async = true;
     googleApiScript.defer = true;
     window.document.body.appendChild(googleApiScript);
     googleApiScript.addEventListener('load', () => {
-      window.gapi.load('client:auth2', () => initClient(googleSigningButton.current, googleSignoutButton.current,(response)=>{
+      window.gapi.load('client:auth2', () => initClient(googleSigningButton.current, googleSignoutButton.current, (response) => {
         console.log(response.result);
-        setCalendars(response.result.items);
+        setCalendars(response.result.items || []);
       }));
     });
-  },[]);
+  }, []);
 
-
-
+  //eslint-disable-next-line
   return (
-    <div className="App">
-        <button id="signin" ref={googleSigningButton} > signin</button>
-        <button id="signout" ref={googleSignoutButton} > signout</button>
-        <GCalendarList calendars={calendars}></GCalendarList>
+    <div>
+      <Navigation signinButton={googleSigningButton} signoutButton={googleSignoutButton}></Navigation>
+      <div className="container">
+        <div class="row">
+          <div class="col s12 m6">
+            <GCalendarList calendars={calendars}></GCalendarList>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
